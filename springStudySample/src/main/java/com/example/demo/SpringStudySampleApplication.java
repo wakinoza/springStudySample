fce1d66c32fe1cde976502490ffa57cb49d0a7c0
wakinoza <wakinoza@gmail.com>
2026-03-23 15:20:48 +0900

    DIの練習

--- springStudySample/src/main/java/com/example/demo/SpringStudySampleApplication.java
+++ springStudySample/src/main/java/com/example/demo/SpringStudySampleApplication.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import com.example.demo.used.Greet;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SpringStudySampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringStudySampleApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringStudySampleApplication.class, args)
+        .getBean(SpringStudySampleApplication.class).execute();
+  }
+
+  @Autowired
+  private Greet g;
+
+  private void execute() {
+    String msg = g.greeting();
+    System.out.println(msg);
+  }
 
 }
---
+++ springStudySample/src/main/java/com/example/demo/used/MoningGreet.java
@@ -0,0 +1,9 @@
+package com.example.demo.used;
+
+// @Component
+public class MoningGreet implements Greet {
+  @Override
+  public String greeting() {
+    return "おはようございます";
+  }
+}
