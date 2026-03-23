5d4a25c7eb881552479ae1d46e08f5e40bfd4447
wakinoza <wakinoza@gmail.com>
2026-03-23 14:30:29 +0900

    Lombokの練習 @Data,@AllArgsConstrucotr,@NoArgsConstructor

--- springStudySample/src/main/java/com/example/demo/entity/User.java
+++ springStudySample/src/main/java/com/example/demo/entity/User.java
@@ -1,10 +1,12 @@
 package com.example.demo.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
-@Setter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
   private String name;
   private int age;
