81ba0104847651d074746fddb713d575c13dd3df
wakinoza <wakinoza@gmail.com>
2026-03-25 15:03:42 +0900

    thymeleafの練習

--- springStudySample/src/main/java/com/example/demo/controller/HelloViewController.java
+++ springStudySample/src/main/java/com/example/demo/controller/HelloViewController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,5 +12,11 @@ public class HelloViewController {
   @GetMapping("View")
   public String helloView() {
     return "Hello";
+  }
+
+  @GetMapping("Model")
+  public String helloView(Model model) {
+    model.addAttribute("msg", "タイムリーフ");
+    return "HelloThymeleaf";
   }
 }
