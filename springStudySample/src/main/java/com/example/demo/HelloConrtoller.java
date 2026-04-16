bb03a8d1e57eadb0eebb6b0cd5df05c378dabc6d
wakinoza <wakinoza@gmail.com>
2026-04-16 14:59:33 +0900

    @PathVariableを使ったhtmlの振り分けの練習

--- springStudySample/src/main/java/com/example/demo/HelloConrtoller.java
+++ springStudySample/src/main/java/com/example/demo/HelloConrtoller.java
@@ -1,12 +1,18 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloConrtoller {
-  @RequestMapping("/")
-  public String index() {
-    return "index";
+  @RequestMapping("/{temp}")
+  public String index(@PathVariable String temp) {
+    return switch (temp) {
+      case "index" -> "index";
+      default -> "other";
+    };
   }
+
+
 }
