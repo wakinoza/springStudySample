64f0d2b3d86315e2c8edefa96c96dc66b58c3dd8
wakinoza <wakinoza@gmail.com>
2026-04-17 11:52:22 +0900

    変数からTｈymeleafへの値の受け渡し

--- springStudySample/src/main/java/com/example/demo/HelloConrtoller.java
+++ springStudySample/src/main/java/com/example/demo/HelloConrtoller.java
@@ -2,13 +2,18 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloConrtoller {
-  @RequestMapping()
-  public String index(Model model) {
-    model.addAttribute("msg", "これはコントローラーに用意したメッセージです。");
+  @RequestMapping("/{num}")
+  public String index(@PathVariable int num, Model model) {
+    int res = 0;
+    for (int i = 1; i <= num; i++) {
+      res += i;
+    }
+    model.addAttribute("msg", "totao:" + res);
     return "index";
   }
 
