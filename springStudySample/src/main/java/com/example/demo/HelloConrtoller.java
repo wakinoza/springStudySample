f7363fd9b4dd64e74f598c9fabc70b4ba6f65e2e
wakinoza <wakinoza@gmail.com>
2026-04-17 11:48:45 +0900

    Thymeleafの練習

--- springStudySample/src/main/java/com/example/demo/HelloConrtoller.java
+++ springStudySample/src/main/java/com/example/demo/HelloConrtoller.java
@@ -1,17 +1,15 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloConrtoller {
-  @RequestMapping("/{temp}")
-  public String index(@PathVariable String temp) {
-    return switch (temp) {
-      case "index" -> "index";
-      default -> "other";
-    };
+  @RequestMapping()
+  public String index(Model model) {
+    model.addAttribute("msg", "これはコントローラーに用意したメッセージです。");
+    return "index";
   }
 
 
