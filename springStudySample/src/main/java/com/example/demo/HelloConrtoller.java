cd38656b22905fb8e3224ccf35bc298acc6837eb
wakinoza <wakinoza@gmail.com>
2026-04-20 15:20:00 +0900

    フォームコントロールの練習

--- springStudySample/src/main/java/com/example/demo/HelloConrtoller.java
+++ springStudySample/src/main/java/com/example/demo/HelloConrtoller.java
@@ -12,16 +12,31 @@ public class HelloConrtoller {
   @RequestMapping(value = "/", method = RequestMethod.GET)
   public ModelAndView index(ModelAndView mav) {
 
-    mav.addObject("msg", "名前を書いてください");
+    mav.addObject("msg", "  フォームを送信ください");
     mav.setViewName("index");
     return mav;
   }
 
   @RequestMapping(value = "/", method = RequestMethod.POST)
-  public ModelAndView form(@RequestParam("text1") String str, ModelAndView mav) {
+  public ModelAndView form(@RequestParam(value = "check1", required = false) boolean check1,
+      @RequestParam(value = "radio1", required = false) String radio1,
+      @RequestParam(value = "select1", required = false) String select1,
+      @RequestParam(value = "select2", required = false) String[] select2, ModelAndView mav) {
 
-    mav.addObject("msg", "こんにちは" + str + "さん");
-    mav.addObject("value", str);
+    String res = "";
+    try {
+      res = "check:" + check1 + " radio:" + radio1 + " select:" + select1 + "\nselect2:";
+    } catch (NullPointerException e) {
+    }
+    try {
+      res += select2[0];
+      for (int i = 1; i < select2.length; i++) {
+        res += "," + select2[i];
+      }
+    } catch (NullPointerException e) {
+      res += "null";
+    }
+    mav.addObject("msg", res);
     mav.setViewName("index");
     return mav;
   }
