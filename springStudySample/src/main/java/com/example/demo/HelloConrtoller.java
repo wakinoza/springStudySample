7b828b60405ecd07c42688e4022b1d0459b55a82
wakinoza <wakinoza@gmail.com>
2026-04-21 14:59:37 +0900

    th:ifとth:unlessの練習

--- springStudySample/src/main/java/com/example/demo/HelloConrtoller.java
+++ springStudySample/src/main/java/com/example/demo/HelloConrtoller.java
@@ -2,16 +2,18 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 public class HelloConrtoller {
-  @RequestMapping(value = "/", method = RequestMethod.GET)
-  public ModelAndView index(ModelAndView mav) {
+  private boolean flag = false;
 
-    mav.addObject("msg", "HelloController/indexのページです");
+  @RequestMapping("/")
+  public ModelAndView index(ModelAndView mav) {
+    flag = !flag;
+    mav.addObject("flag", flag);
+    mav.addObject("msg", "サンプルのメッセージです");
     mav.setViewName("index");
     return mav;
   }
