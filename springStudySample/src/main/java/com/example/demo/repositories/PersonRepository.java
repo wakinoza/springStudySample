5c9cdcb52b7cd38f48cd25603b539f0c47fe0c61
wakinoza <wakinoza@gmail.com>
2026-05-06 15:20:53 +0900

    データの修正

--- springStudySample/src/main/java/com/example/demo/repositories/PersonRepository.java
+++ springStudySample/src/main/java/com/example/demo/repositories/PersonRepository.java
@@ -1,10 +1,12 @@
 package com.example.demo.repositories;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.demo.entities.Person;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
-
+  @Override
+  Optional<Person> findById(Long name);
 }
